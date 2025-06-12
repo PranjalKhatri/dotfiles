36b2c2f75bf11a46a3df5dc861ddf61a6b704d44
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-06-12 23:08:38 +0530

    nice looking black bg

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -2,8 +2,26 @@ return {
 	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
+	opts = {
+		style = "night",
+		transparent = false,
+		styles = {
+			sidebars = "dark",
+			floats = "dark",
+		},
+		on_colors = function(colors)
+			colors.bg = "#000000"
+			colors.bg_dark = "#000000"
+			colors.bg_float = "#000000"
+			colors.bg_sidebar = "#000000"
+			colors.bg_statusline = "#000000"
+		end,
+	},
 	init = function()
-		vim.cmd.colorscheme 'tokyonight-night'
-		vim.cmd.hi 'Comment gui=none'
+		vim.cmd.colorscheme("tokyonight-night")
+		vim.cmd.hi("Comment gui=none")
+		vim.cmd.hi("Normal guibg=#000000")
+		vim.cmd.hi("NormalFloat guibg=#000000")
+		vim.cmd.hi("NormalNC guibg=#000000")
 	end,
 }
